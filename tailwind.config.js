1155c423242071f407ce8458ee1f65f7ee56cb57
Akshat9080 <akshat.gohil2020@gmail.com>
2023-12-29 10:43:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Nato:["Noto Sans JP","sans-serif"]
+      },
+      colors:{
+        lightcolor:"#7B68EE",
+        lightBlue:"#2563eb",
+        Blue:"#1d4ed8",
+        deepBlue:"#16161d",
+        lightBlack:"#282C35",
+      }
+    },
+  },
+  plugins: [],
+}
+
